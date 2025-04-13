5ca64024fb546ad3ff075087c53680f6b8dd3b32
wicaksonoleksono <wcksnlxn@gmail.com>
2025-04-13 12:28:02 +0700

    [13.04.25] A. Wicaksono | ✨ feat : "new 2 distance"

--- src/modules/Losses/Triplet/SentTrip.py
+++ src/modules/Losses/Triplet/SentTrip.py
@@ -32,6 +32,11 @@ class SentenceTriplet(nn.Module):
         safe_sim = torch.clamp(sim_matrix, -1.0 + eps, 1.0 - eps)
         return torch.acos(safe_sim)
 
+    def _chord_distance(self, x, y):
+        # r =1 karena sudah ternormalisasi
+        theta = self._angular_distance(x, y)
+        return 2*torch.sin(theta/2)
+
     def _correlation_distance(self, x, y):
         sim_matrix = self._cosine_sim(x, y)
         return torch.sqrt((1 - sim_matrix)/2)
@@ -73,6 +78,9 @@ class SentenceTriplet(nn.Module):
         elif self.d_fn == "angular":
             d_ap = self._angular_distance(og_feat, ag_feat).diag()  # diagonal
             d_an = self._angular_distance(og_feat, og_feat)
+        elif self.d_fn == "chord":
+            d_ap = self._chord_distance(og_feat, ag_feat).diag()  # diagonal
+            d_an = self._chord_distance(og_feat, og_feat)
         elif self.d_fn == "correlation":
             d_ap = self._correlation_distance(
                 og_feat, ag_feat).diag()  # diagonal
