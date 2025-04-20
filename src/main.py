b5a5e1cfffda9ba55464b3b63c22979a83bd90de
wicaksonoleksono <wcksnlxn@gmail.com>
2025-04-20 11:11:19 +0700

    [20.04.25] A. Wicaksono | ✨ feat : "abc"

--- src/main.py
+++ src/main.py
@@ -161,7 +161,7 @@ def main(args=None):
         else:
             raise ValueError(f"Unsupported method: {combo['method']}")
 
-    if combo['method'] == "semi_hard":
+    if combo['method'] == "semi_hard" or "SST":
         combo['method_dir'] = f"{output_base}.{combo['method']}.{combo['data_main']}.{encoder_short_name}.{combo['distance_fn']}.{combo['reducer']}"
         os.makedirs(combo['method_dir'], exist_ok=True)
     elif combo['method'] == "contrastive":
@@ -203,7 +203,6 @@ def main(args=None):
             num_epochs=combo["num_epochs"],
             lambda_weight=combo["lambda_weight"],
             method=combo["method"],
-
             # triplet loss
             # if combo["method"] == "semi-hard" else None,
             d_fn=combo["distance_fn"],
